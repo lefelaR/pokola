4d9b3fc5dc1f81208a26b56edf8e7bdd127cf44e
lefelaR <rakgew@gmail.com>
2025-06-24 21:34:16 +0200

    defwrew

--- Mobile/rider/backend/src/functions/users/addUser/schema.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  type: "object",
-  properties: {
-    id: { type: "string" },
-    name: { type: "string" },
-    phoneNumber: { type: "string" },
-  },
-  required: ["id", "name", "phoneNumber"],
-} as const;
